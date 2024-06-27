b41a541d65a8173e8c47901b949b48c8eb4d4479
HITMAN <abdulraufmuhammad28@gmail.com>
2024-06-27 20:00:11 +0200

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById('btn').onclick=function(){
+    document.getElementById('form').style.display='block';
+}
+document.getElementById('cancel').onclick=function(){
+    document.getElementById('form').style.display='none';
+}
+// SIGN UP
+document.getElementById('bn').onclick=function(){
+    document.getElementById('fom').style.display='block';
+}
+document.getElementById('cance').onclick=function(){
+    document.getElementById('fom').style.display='none';
+}
+
